88685df8e73d9cc8103ba3794fdd1128b55ed3ae
VictorSundberg1 <victor.sundberg1@hotmail.com>
2025-05-05 12:08:08 +0200

    Re-made the habit model and viewmodel to fix streak resetting, also updated the listview of the homescreen and added blank profile and overview views to the tab-view

---
+++ Streakr/Views/HabitCardView.swift
@@ -0,0 +1,18 @@
+//
+//  HabitCardView.swift
+//  Streakr
+//
+//  Created by Victor Sundberg on 2025-05-05.
+//
+
+import SwiftUI
+
+struct HabitCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HabitCardView()
+}
